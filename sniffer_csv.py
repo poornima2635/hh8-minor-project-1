7dc876772f24868a285473b3c3fc4b525390c131
poornima2635 <poornimap8055@gmail.com>
2026-01-08 18:55:27 +0530

    Added CSV logging feature to packet sniffer.

---
+++ sniffer_csv.py
@@ -0,0 +1,25 @@
+from scapy.all import sniff, IP, TCP, UDP, ICMP 
+import csv
+
+file = open("captured_packet.csv", mode="w", newline="")
+writer = csv.writer(file)
+writer.writerow(["Source IP", "Destination IP", "Protocol"])
+
+def process_packet(packet):
+    if IP in packet:
+        src = packet[IP].src
+        dst = packet[IP].dst
+
+        protocol = "OTHER"
+        if TCP in packet:
+            protocol = "TCP"
+        elif UDP in packet:
+            protocol = "UDP"
+        elif ICMP in packet:
+            protocol = "ICMP"
+
+        print(f"{src} --> {dst} | {protocol}")
+
+        writer.writerow([src, dst, protocol])
+
+sniff(prn=process_packet, count=100)
