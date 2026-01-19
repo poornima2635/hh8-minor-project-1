334b3efd89150f9f79e62e5767640be531adae13
poornima2635 <poornimap8055@gmail.com>
2026-01-19 12:58:55 +0530

    Added CSV support to save captured packet details

--- sniffer_csv.py
+++ sniffer_csv.py
@@ -1,23 +1,24 @@
-from scapy.all import sniff, IP, TCP, UDP 
+from scapy.all import sniff, IP, TCP, UDP, ICMP
 import csv
 
-file = open("captured_packet.csv", mode="w", newline="")
-writer = csv.writer(file)
-writer.writerow(["Source IP", "Destination IP", "Protocol"])
+with open("captured_packet.csv", mode="w", newline="") as file:
+    writer = csv.writer(file)
+    writer.writerow(["Source IP", "Destination IP", "Protocol"])
 
-def process_packet(packet):
-    if IP in packet:
-        src = packet[IP].src
-        dst = packet[IP].dst
+    def process_packet(packet):
+        if IP in packet:
+            src = packet[IP].src
+            dst = packet[IP].dst
 
-        protocol = "OTHER"
-        if TCP in packet:
-            protocol = "TCP"
-        elif UDP in packet:
-            protocol = "UDP"
+            protocol = "OTHER"
+            if TCP in packet:
+                protocol = "TCP"
+            elif UDP in packet:
+                protocol = "UDP"
+            elif UDP in packet:
+                protocol = "ICMP"
 
-        print(f"{src} --> {dst} | {protocol}")
+            writer.writerow([src, dst, protocol])
+            print(f"{src} -> {dst} | {protocol}")
 
-        writer.writerow([src, dst, protocol])
-
-sniff(prn=process_packet, count=100)
+    sniff(prn=process_packet, count=100)
